bdd2aea10a865028dea097633367f0ae3a534495
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2023-05-03 08:22:37 +0100

    Created bash script to install docker, docker compose plugin and docker-compose standalone

---
+++ docker-installation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+
+# Add Docker’s official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# set up the repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+#Install Docker Engine, containerd, and Docker Compose
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -a -G docker ubuntu
+
+## Install the Compose standalone
+
+# download and install Compose standalone, run
+curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt-get update
